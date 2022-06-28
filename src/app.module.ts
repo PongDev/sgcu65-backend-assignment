d42290af53c824b2a2ad762b301a58f517271af5
PongDev <pongwiwat.dev@outlook.com>
2022-06-28 17:26:26 +0700

    Scaffolding Project Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from 'config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { TeamModule } from './team/team.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +14,10 @@ import { AppService } from './app.service';
       isGlobal: true,
       load: [configuration],
     }),
+    UserModule,
+    TaskModule,
+    TeamModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
